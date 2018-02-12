502a81a2a8da0563ce9dc88253727eb81d5b02be
Josh Brackett <joshua.brackett@hotmail.ca>
2018-02-12 15:18:57 -0500

    got the knock back working with a multiplier so the more you're hit the farther you get knocked back

---
+++ BallzArena/Assets/Scripts/KnockBack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour {
+
+	public float m_knockBackAmount = 5.0f;
+	public float m_knockBackMultiplier = 1.5f;
+	public Rigidbody m_rb;
+	public bool m_isKnocked;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.name == "PlayerBullet(Clone)" && other.tag != this.tag) {
+			m_isKnocked = true;
+			Vector2 dir = other.transform.position - transform.position;
+			dir.Normalize();
+			m_rb.AddForce(-dir * m_knockBackAmount, ForceMode.Impulse);
+			m_knockBackAmount *= m_knockBackMultiplier;
+			StartCoroutine(IsKnockedBack());
+			Destroy(other.gameObject);
+		}
+	}
+
+	IEnumerator IsKnockedBack() {
+        yield return new WaitForSeconds(.5f);
+		m_isKnocked = false;
+    }
+}
--- BallzArena/Assets/Scripts/Player.cs
+++ BallzArena/Assets/Scripts/Player.cs
@@ -6,7 +6,6 @@ public class Player : MonoBehaviour {
 
 	public float m_speed = 10.0f;
 	public float m_baseJumpForce = 10.0f;
-	public float m_secondThirdJumpForce = 10.0f;
 	public float m_bulletSpeed = 6.0f;
 	public GameObject m_bulletPrefab;
 	public GameObject m_bulletSpawn;
@@ -16,29 +15,29 @@ public class Player : MonoBehaviour {
 	private int m_numOfJumps = 3;
 	private int m_currNumOfJumps;
 	private bool m_canShoot = true;
+	private KnockBack m_knockBackScript;
 
 	void Awake() {
 		m_rb = GetComponent<Rigidbody>();
+		m_knockBackScript = GetComponent<KnockBack>();
 		m_currNumOfJumps = m_numOfJumps;
 	}
 
 	void Update() {
-		if(Input.GetKey(KeyCode.A)) {
-			transform.Translate(Vector3.left * Time.deltaTime * m_speed);
-		} 
+		if(!m_knockBackScript.m_isKnocked) {
+			if(Input.GetKey(KeyCode.A)) {
+				transform.Translate(Vector3.left * Time.deltaTime * m_speed);
+			} 
 
-		if(Input.GetKey(KeyCode.D)) {
-			transform.Translate(Vector3.right * Time.deltaTime * m_speed);
-		}
-
-		if(Input.GetButtonDown("Jump") && m_currNumOfJumps > 0) {
-			if(m_rb.velocity.y < 0) {
-				m_rb.AddForce(Vector3.up * m_secondThirdJumpForce, ForceMode.Impulse);
-			} else {
-				m_rb.AddForce(Vector3.up * m_baseJumpForce, ForceMode.Impulse);
+			if(Input.GetKey(KeyCode.D)) {
+				transform.Translate(Vector3.right * Time.deltaTime * m_speed);
 			}
-			m_isGrounded = false;
-			m_currNumOfJumps--;
+
+			if(Input.GetButtonDown("Jump") && m_currNumOfJumps > 0) {
+				m_rb.velocity = Vector3.up * m_baseJumpForce;
+				m_isGrounded = false;
+				m_currNumOfJumps--;
+			}
 		}
 
 		if(Input.GetMouseButtonDown(0) && m_canShoot) {
@@ -50,11 +49,12 @@ public class Player : MonoBehaviour {
 
 	void Fire() {
 		Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(Input.mousePosition.x,  Input.mousePosition.y) );
-		Vector2 myPos = new Vector2(m_bulletSpawn.transform.position.x, m_bulletSpawn.transform.position.y);
+		Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
 		Vector2 direction = target - myPos;
 		direction.Normalize();
 		Quaternion rotation = Quaternion.Euler( 0, 0, Mathf.Atan2 ( direction.y, direction.x ) * Mathf.Rad2Deg );
-		GameObject bullet = (GameObject) Instantiate(m_bulletPrefab, myPos, rotation);
+		GameObject bullet = (GameObject)Instantiate(m_bulletPrefab, myPos, rotation);
+		bullet.tag = gameObject.tag;
 	 	bullet.GetComponent<Rigidbody>().velocity = direction * m_bulletSpeed;
 
 		Destroy(bullet, 2.0f);
