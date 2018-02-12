5f7d7baaff619be9ba6981ccd40c1273f6fab8e5
Josh Brackett <joshua.brackett@hotmail.ca>
2018-02-12 15:23:26 -0500

    added a multiplier to the IsKnockedBack so your movement is disabled depending on how far you're knocked back

--- BallzArena/Assets/Scripts/KnockBack.cs
+++ BallzArena/Assets/Scripts/KnockBack.cs
@@ -6,6 +6,7 @@ public class KnockBack : MonoBehaviour {
 
 	public float m_knockBackAmount = 5.0f;
 	public float m_knockBackMultiplier = 1.5f;
+	public float m_knockBackWaitTime = 0.5f;
 	public Rigidbody m_rb;
 	public bool m_isKnocked;
 
@@ -22,7 +23,8 @@ public class KnockBack : MonoBehaviour {
 	}
 
 	IEnumerator IsKnockedBack() {
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(m_knockBackWaitTime);
+		m_knockBackWaitTime *= m_knockBackMultiplier;
 		m_isKnocked = false;
     }
 }
