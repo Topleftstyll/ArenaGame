43075a16d2153a0914a55ec41b005d774f2cc817
Josh Brackett <joshua.brackett@hotmail.ca>
2018-02-14 14:23:55 -0500

    bullets now get destroyed from the server with CmdDestroy

--- BallzArena/Assets/Scripts/Player.cs
+++ BallzArena/Assets/Scripts/Player.cs
@@ -50,11 +50,11 @@ public class Player : NetworkBehaviour {
 
 	[Command]
 	void CmdFire() {
-		Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(Input.mousePosition.x,  Input.mousePosition.y) );
+		Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
 		Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
 		Vector2 direction = target - myPos;
 		direction.Normalize();
-		Quaternion rotation = Quaternion.Euler( 0, 0, Mathf.Atan2 ( direction.y, direction.x ) * Mathf.Rad2Deg );
+		Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
 		GameObject bullet = (GameObject)Instantiate(m_bulletPrefab, myPos, rotation);
 		bullet.tag = "Player";
 	 	bullet.GetComponent<Rigidbody>().velocity = direction * m_bulletSpeed;
